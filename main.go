0f1416bd6fbe31acbce64d8a1fa056bd7f469748
keita.takaue <keita.takaue@gmail.com>
2023-01-13 19:40:35 +0900

    引数付きの関数の追加

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func outer() {
+	fmt.Println("time.Now()")
+}
+
 // HelloWorld
 func main() {
+	// console.log的な
+	// timeをimportしてdate now()的な
+	fmt.Println(time.Now())
+	var kevin string = "kevin";
+	fmt.Println(kevin)
+	var hello string = "hello world Go";
+	fmt.Println(hello)
+	// 2ついれる
+	var t,f bool = true, false
+	fmt.Println(t)
+	fmt.Println(f)
+	var (
+		a string = "A"
+		b string = "B"
+	)
+	fmt.Println(a,b)
+	var tetete  string = "kevin"
+	fmt.Println(tetete)
+	outer()
+	fmt.Println(rand.Intn(10))
+}
 
+func kevin(x, y int)int {
+	return x + y
 }
