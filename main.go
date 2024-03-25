a2d4be26485be4a97c99f480fcc05ef3fb9abe92
kevin(けいた) <75979960+ktakaue@users.noreply.github.com>
2024-03-25 19:20:14 +0900

    add: 変数追加

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "time"
-
 func main(){
-	println("Hello, World!")
-	print(time.Now().String())
+	// println("Hello, World!")
+	// print(time.Now().String())
+
+	// 適当に変数を定義する
+	var a int = 1
+	b  := 2
+	var c, d int = 3, 4
+	println(a, b, c, d)
+
+	e,f,g,h := 5,6,7,8
+	// 改行しながら出力
+	println(e)
+	println(f)
+	println(g)
+	println(h)
+
 }
